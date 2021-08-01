2652a2f07472494aa3e6a84d2216014482c86851
TahaOuarrak <tahaouarrak@gmail.com>
2021-08-02 00:58:15 +0100

    Fix resetting lossRecovery multiplier on all-symbol save

--- app/helpers/mongo.js
+++ app/helpers/mongo.js
@@ -40,6 +40,18 @@ const findOne = async (funcLogger, collectionName, query) => {
   return result;
 };
 
+const findAll = async (funcLogger, collectionName, query) => {
+  const logger = funcLogger.child({ helper: 'mongo', funcName: 'findAll' });
+
+  const collection = database.collection(collectionName);
+
+  logger.info({ collectionName, query }, 'Finding documents from MongoDB');
+  const result = await collection.find(query).toArray();
+  logger.info({ result }, 'Found documents from MongoDB');
+
+  return result;
+};
+
 const insertOne = async (funcLogger, collectionName, document) => {
   const logger = funcLogger.child({ helper: 'mongo', funcName: 'insertOne' });
 
@@ -71,6 +83,44 @@ const upsertOne = async (funcLogger, collectionName, filter, document) => {
   return result;
 };
 
+const upsertAll = async (funcLogger, collectionName, filter, document) => {
+  const logger = funcLogger.child({ helper: 'mongo', funcName: 'upsertAll' });
+
+  const collection = database.collection(collectionName);
+
+  logger.info(
+    { collectionName, filter, document },
+    'Upserting documents to MongoDB'
+  );
+  const result = await collection.updateMany(
+    filter,
+    { $set: document },
+    { upsert: true }
+  );
+  logger.info({ result }, 'Upserted documents to MongoDB');
+
+  return result;
+};
+
+const deleteFields = async (
+  funcLogger,
+  collectionName,
+  filter,
+  fieldsToDelete
+) => {
+  const logger = funcLogger.child({
+    helper: 'mongo',
+    funcName: 'deleteFields'
+  });
+
+  logger.info({ collectionName, filter }, 'Deleting fields from MongoDB');
+  const collection = database.collection(collectionName);
+  const result = collection.updateMany(filter, { $unset: fieldsToDelete });
+  logger.info({ result }, 'Deleted fields from MongoDB');
+
+  return result;
+};
+
 const deleteAll = async (funcLogger, collectionName, filter) => {
   const logger = funcLogger.child({ helper: 'mongo', funcName: 'deleteAll' });
 
@@ -96,8 +146,11 @@ const deleteOne = async (funcLogger, collectionName, filter) => {
 module.exports = {
   connect,
   findOne,
+  findAll,
   insertOne,
+  upsertAll,
   upsertOne,
   deleteAll,
+  deleteFields,
   deleteOne
 };
